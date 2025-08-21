5fcd3cc090d8e06fea8fadaee2dee6f642925315
0xeae3f7c0 <0xeae3f7c0@proton.me>
2025-08-21 07:55:16 +0100

    feat(gnome): enable fractional scaling

--- nixos/modules/desktop-gnome.nix
+++ nixos/modules/desktop-gnome.nix
@@ -1,7 +1,23 @@
-{
+{ pkgs, ... }: {
   services.xserver = {
     enable = true;
-    displayManager.gdm.enable = true;
-    desktopManager.gnome.enable = true;
+
+    displayManager.gdm = {
+      enable = true;
+      wayland = true;
+    };
+
+    desktopManager.gnome = {
+      enable = true;
+
+      # Ensure the override applies to Mutter’s schema
+      extraGSettingsOverridePackages = [ pkgs.mutter ];
+
+      # Enable fractional scaling and sharper XWayland app behaviour
+      extraGSettingsOverrides = ''
+        [org.gnome.mutter]
+        experimental-features=['scale-monitor-framebuffer','xwayland-native-scaling']
+      '';
+    };
   };
 }
